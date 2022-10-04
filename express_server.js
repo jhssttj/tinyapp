ca988814d72b78c7b7bbc2b51b155a20e5877300
Kevin Lee <jhssttj@hotmail.com>
2022-10-04 16:57:30 -0600

    Added cookie parser, login input and username templates

--- express_server.js
+++ express_server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -25,7 +27,7 @@ app.get("/", (req, res) => {
 });
 //Url Index Page: Shows all of the long and short URL and delete button
 app.get("/urls", (req, res) => {
-  const templateVars = {urls: urlDatabase};
+  const templateVars = {urls: urlDatabase, username: req.cookies["username"]};
   res.render("urls_index", templateVars);
 });
 //Creates new URL and short code for it
@@ -43,11 +45,12 @@ app.post("/urls/:id", (req, res) => {
 });
 //New URL page: Input new url to add to the database
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {username: req.cookies["username"]};
+  res.render("urls_new", templateVars);
 });
 //Specific URL page
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies["username"] };
   res.render("urls_show", templateVars);
 });
 //URL Json page
@@ -66,6 +69,11 @@ app.get("/u/:id", (req, res) => {
 //Delete requested URL once the delete button is clicked
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
+  res.redirect('/urls');
+});
+//Login Button logs you in and redirect back to URL page
+app.post("/login", (req, res) => {
+  res.cookie('username', req.body.username);
   res.redirect('/urls');
 });
 
